4742c02c950f563412de1c40a0b90b7b054aa6ea
Luka <nickolas.kanno@gmail.com>
2022-08-23 12:41:05 -0700

    add clients cache update

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -1,11 +1,41 @@
 import { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
+import { ADD_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
+
 
 export default function AddClientModal() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
+
+    const [addClient] = useMutation(ADD_CLIENT, {
+        variables: { name, email, phone },
+        update(cache, { data: { addClient } }) {
+            const { clients } = cache.readQuery({ query:
+            GET_CLIENTS });
+
+            cache.writeQuery({
+                query: GET_CLIENTS,
+                data: { clients: [...clients, addClient] },
+            });
+        }
+    });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        
+        if(name ==='' || email === '' || phone === '' ) {
+            return alert('Please fill in all fields');
+        }
+
+        addClient(name, email, phone);
+
+        setName('');
+        setEmail('');
+        setPhone('');
+    };
 
   return (
     <>
@@ -42,7 +72,7 @@ aria-hidden="true"
         </button>
       </div>
       <div className="modal-body">
-          <form>
+          <form onSubmit={onSubmit}>
 
               <div className="mb-3">
                   <label className="form-label">Name
@@ -79,6 +109,11 @@ aria-hidden="true"
                   onChange={(e) => setPhone(e.target.value)} 
                   />
               </div>
+
+              <button 
+              type="submit"
+              data-bs-dismiss="modal"
+              className="btn btn-secondary">Submit</button>
           </form>
       </div>
      
